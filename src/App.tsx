542a8b20c24dd8fa1f5da1395ef7125f277df724
Buvani Pai <bhuvaniprakashpai@gmail.com>
2025-12-20 17:58:12 -0600

    removed unused libs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
